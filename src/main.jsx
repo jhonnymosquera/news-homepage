add20091d0e426c509ff28c1b76c3bdd254d44c2
jhonnymosquera <78888142+jhonnymosquera@users.noreply.github.com>
2022-11-10 08:24:18 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import ContexProvider from './context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<ContexProvider>
 		<React.StrictMode>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</React.StrictMode>
 	</ContexProvider>
 );
