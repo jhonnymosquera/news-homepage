cc450978f1d396b6a1ed3231c5fc3ecd55e94c3d
Jhonny Mosquera <78888142+jhonnymosquera@users.noreply.github.com>
2023-03-23 20:14:03 -0500

    update

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,35 +1,36 @@
-import useAppContext from '../../context/useAppContext';
-import NavBarLinks from './NavBarLinks';
+import useAppContext from "../../context/useAppContext";
+import NavBarLinks from "./NavBarLinks";
 
 function NavBar() {
-	const { logo, routes, menuActive, menuDisplay, handleButton, isMobile, Link } = useAppContext();
+  const { routes, menuActive, menuDisplay, handleButton, isMobile, Link } =
+    useAppContext();
 
-	return (
-		<header className="header">
-			<div className="logo">
-				<Link to="/">
-					<img src={logo} alt="logo" />
-				</Link>
-			</div>
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
+      </div>
 
-			{isMobile && (
-				<div className="menu-button" onClick={handleButton}>
-					<img src={menuActive} alt="menu button" />
-				</div>
-			)}
+      {isMobile && (
+        <div className="menu-button" onClick={handleButton}>
+          <img src={menuActive} alt="menu button" />
+        </div>
+      )}
 
-			<nav
-				className={`${isMobile ? 'navbar' : 'navbar-desk'} ${menuDisplay}`}
-				onClick={handleButton}
-			>
-				<ul className="navbar-ul">
-					{routes.map((route) => (
-						<NavBarLinks key={route.id} route={route} />
-					))}
-				</ul>
-			</nav>
-		</header>
-	);
+      <nav
+        className={`${isMobile ? "navbar" : "navbar-desk"} ${menuDisplay}`}
+        onClick={handleButton}
+      >
+        <ul className="navbar-ul">
+          {routes.map((route) => (
+            <NavBarLinks key={route.id} route={route} />
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar;
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,50 +1,48 @@
-import { Context } from './Context';
-import news from '../data/news.json';
-import popular from '../data/popular';
-import trending from '../data/trending';
-import logo from '../../public/logo.svg';
-import routes from '../data/ruotes.json';
-import menuClose from '../../public/icon-menu-close.svg';
-import menu from '../../public/icon-menu.svg';
-import buildingPage from '../assets/img/website-construction.jpg';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Context } from "./Context";
+import news from "../data/news.json";
+import popular from "../data/popular";
+import trending from "../data/trending";
+import routes from "../data/ruotes.json";
+import menuClose from "../../public/icon-menu-close.svg";
+import menu from "../../public/icon-menu.svg";
+import buildingPage from "../assets/img/website-construction.jpg";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function ContexProvider({ children }) {
-	const [menuState, setMenuState] = useState(false);
-	const menuActive = menuState ? menuClose : menu;
-	const menuDisplay = menuState ? 'menu-active' : '';
+  const [menuState, setMenuState] = useState(false);
+  const menuActive = menuState ? menuClose : menu;
+  const menuDisplay = menuState ? "menu-active" : "";
 
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-	const isMobile = windowWidth < 475;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isMobile = windowWidth < 475;
 
-	const handleButton = () => {
-		setMenuState(!menuState);
-	};
+  const handleButton = () => {
+    setMenuState(!menuState);
+  };
 
-	useEffect(() => {
-		window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
-	}, []);
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, []);
 
-	return (
-		<Context.Provider
-			value={{
-				news,
-				popular,
-				trending,
-				logo,
-				routes,
-				menuActive,
-				menuDisplay,
-				handleButton,
-				buildingPage,
-				isMobile,
-				Link,
-			}}
-		>
-			{children}
-		</Context.Provider>
-	);
+  return (
+    <Context.Provider
+      value={{
+        news,
+        popular,
+        trending,
+        routes,
+        menuActive,
+        menuDisplay,
+        handleButton,
+        buildingPage,
+        isMobile,
+        Link,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
 
 export default ContexProvider;
